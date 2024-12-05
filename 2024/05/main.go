33b681992c4438ffda6783aad47bd5707ce46bcf
Daniele Muffato <danixgithub1@gmail.com>
2024-12-05 15:04:36 +0100

    ...

--- 2024/05/main.go
+++ 2024/05/main.go
@@ -66,7 +66,7 @@ func correctOrder(precedences [][]int, line []int) int {
 }
 
 func part1() {
-	_, _, precedences, lists := parseFile("example1")
+	_, _, precedences, lists := parseFile("input")
 	acc := 0
 	for _, list := range lists {
 		acc += correctOrder(precedences, list)
