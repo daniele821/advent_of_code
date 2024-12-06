517fd5350912a803649efd94a7f0d606a3b1c119
Daniele Muffato <danixgithub1@gmail.com>
2024-12-06 05:07:16 +0100

    added basic code

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func readLines(file string) []string {
+	lines, _ := os.ReadFile(file)
+	return strings.Split(string(lines), "\n")
+}
+
+func solve(file string) {
+	lines := readLines(file)
+
+	// write code here
+	fmt.Println(lines)
+}
+
+func main() {
+	solve("example")
+	solve("input")
+}
