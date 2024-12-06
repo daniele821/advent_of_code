5cbab54a373e1f13e2e6758a7671dd1cc264434e
Daniele Muffato <danixgithub1@gmail.com>
2024-12-06 05:36:11 +0100

    added template updates to main

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -6,6 +6,21 @@ import (
 	"strings"
 )
 
+func grid(lines []string) [][]string {
+	grid := [][]string{}
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		gridLine := []string{}
+		for _, char := range line {
+			gridLine = append(gridLine, string(char))
+		}
+		grid = append(grid, gridLine)
+	}
+	return grid
+}
+
 func solve(file string) {
 	fileStr, _ := os.ReadFile(file)
 	lines := strings.Split(string(fileStr), "\n")
